5b77f3d9238345e7f66c55fb19b478d9e6baa856
Wayne Black <wblack@smchcn.net>
2013-09-12 13:42:45 -0500

    Final Prime Factor

---
+++ PrimeFinal/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace PrimeFinal
+{
+    public class Prime
+    {
+        public static List<int> Generate(int n)
+        {
+            var primes = new List<int>();
+            for (int candidate = 2;  n > 1; candidate++)
+                for (; n % candidate == 0; n /= candidate)
+                    primes.Add(candidate);
+
+            return primes;
+        }
+    }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimeFinal;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public List<int> MakeList(params int[] ints)
+        {
+            return ints.ToList();
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CollectionAssert.AreEqual(new List<int>(), Prime.Generate(1));
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            CollectionAssert.AreEqual(MakeList(2), Prime.Generate(2));
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            CollectionAssert.AreEqual(MakeList(3), Prime.Generate(3));
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            CollectionAssert.AreEqual(MakeList(2, 2), Prime.Generate(4));
+        }
+
+        [TestMethod]
+        public void Test6()
+        {
+            CollectionAssert.AreEqual(MakeList(2, 3), Prime.Generate(6));
+        }
+
+        [TestMethod]
+        public void Test8()
+        {
+            CollectionAssert.AreEqual(MakeList(2, 2, 2), Prime.Generate(8));
+        }
+
+        [TestMethod]
+        public void Test9()
+        {
+            CollectionAssert.AreEqual(MakeList(3, 3), Prime.Generate(9));
+        }
+    }
+}
